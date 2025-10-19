272398ee58cd953f067ab6e729cb0dfce80ef02d
andersbais <88909455+andersbais@users.noreply.github.com>
2025-10-20 00:26:35 +0200

    Update __init__.py

--- custom_components/stecagrid/__init__.py
+++ custom_components/stecagrid/__init__.py
@@ -53,8 +53,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     }
 
     for platform in PLATFORMS:
-        await hass.async_create_task(
-            hass.config_entries.async_forward_entry_setups(entry, platform)
+        hass.async_create_task(
+            await hass.config_entries.async_forward_entry_setups(entry, platform)
         )
 
     return True
